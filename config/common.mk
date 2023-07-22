e0225cbc12b8260858a4fa8cb6df47a8b4ddd4b0
Mr_ZerO <rbesar210311@gmail.com>
2023-07-22 17:01:34 +0000

    vendor: Build Aperture
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -89,6 +89,10 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(AURORA_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
+# Aperture
+PRODUCT_PACKAGES += \
+    Aperture
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
