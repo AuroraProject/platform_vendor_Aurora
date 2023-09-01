AfterlifeOS/vendor_afterlife

config: Include telephony.mk
Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/common.mk
@@ -183,3 +183,4 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 include vendor/afterlife/config/version.mk
 include vendor/afterlife/config/bootanimation.mk
+include vendor/afterlife/config/telephony.mk

AuroraProject/platform_vendor_Aurora

config/common.mk
@@ -183,3 +183,4 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 include vendor/aurora/config/version.mk
 include vendor/aurora/config/bootanimation.mk
+include vendor/aurora/config/telephony.mk