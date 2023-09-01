DroidX-UI/vendor_droidx

vendor: Add custom fonts
* convert to blueprint and bring all AndroidManifest inline
* included fonts are Google Sans, Inter v3.19, Linotte,
  Manrope v4.504, OnePlus Sans and OnePlus Slate

Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: idoybh <idoybh2@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/fonts.mk
@@ -1,0 +1,70 @@
+# arbutus-slab family
+PRODUCT_PACKAGES += \
+    ArbutusSlab-Regular.ttf
+
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf
+
+# Lustria family
+PRODUCT_PACKAGES += \
+    Lustria-Regular.ttf
+
+# lato family
+PRODUCT_PACKAGES += \
+    Lato-BoldItalic.ttf \
+    Lato-Bold.ttf \
+    Lato-Italic.ttf \
+    Lato-MediumItalic.ttf \
+    Lato-Medium.ttf \
+    Lato-Regular.ttf
+
+# rubik family
+PRODUCT_PACKAGES += \
+    Rubik-BoldItalic.ttf \
+    Rubik-Bold.ttf \
+    Rubik-Italic.ttf \
+    Rubik-MediumItalic.ttf \
+    Rubik-Medium.ttf \
+    Rubik-Regular.ttf
+
+# zilla-slab family
+PRODUCT_PACKAGES += \
+    ZillaSlab-MediumItalic.ttf \
+    ZillaSlab-Medium.ttf \
+    ZillaSlab-SemiBoldItalic.ttf \
+    ZillaSlab-SemiBold.ttf
+
+# Karla family
+PRODUCT_PACKAGES += \
+    Karla-Regular.ttf
+
+# Fraunces family
+PRODUCT_PACKAGES += \
+    Fraunces-Regular.ttf \
+    Fraunces-SemiBold.ttf
+
+# BigShouldersText family
+PRODUCT_PACKAGES += \
+    BigShouldersText-Bold.ttf \
+    BigShouldersText-ExtraBold.ttf
+
+# Barlow family
+PRODUCT_PACKAGES += \
+    Barlow-Bold.ttf \
+    Barlow-Medium.ttf
+	
+#Custom packages
+PRODUCT_PACKAGES += \
+    FontGInterOverlay \
+    FontGoogleSansOverlay \
+    FontHarmonySansOverlay \
+    FontLinotteSourceOverlay \
+    FontManropeOverlay \
+    FontOnePlusSansOverlay \
+    FontOneplusSlateSourceOverlay
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/droidx/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/droidx/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml

AuroraProject/platform_vendor_Aurora

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/fonts.mk
@@ -1,0 +1,70 @@
+# arbutus-slab family
+PRODUCT_PACKAGES += \
+    ArbutusSlab-Regular.ttf
+
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf
+
+# Lustria family
+PRODUCT_PACKAGES += \
+    Lustria-Regular.ttf
+
+# lato family
+PRODUCT_PACKAGES += \
+    Lato-BoldItalic.ttf \
+    Lato-Bold.ttf \
+    Lato-Italic.ttf \
+    Lato-MediumItalic.ttf \
+    Lato-Medium.ttf \
+    Lato-Regular.ttf
+
+# rubik family
+PRODUCT_PACKAGES += \
+    Rubik-BoldItalic.ttf \
+    Rubik-Bold.ttf \
+    Rubik-Italic.ttf \
+    Rubik-MediumItalic.ttf \
+    Rubik-Medium.ttf \
+    Rubik-Regular.ttf
+
+# zilla-slab family
+PRODUCT_PACKAGES += \
+    ZillaSlab-MediumItalic.ttf \
+    ZillaSlab-Medium.ttf \
+    ZillaSlab-SemiBoldItalic.ttf \
+    ZillaSlab-SemiBold.ttf
+
+# Karla family
+PRODUCT_PACKAGES += \
+    Karla-Regular.ttf
+
+# Fraunces family
+PRODUCT_PACKAGES += \
+    Fraunces-Regular.ttf \
+    Fraunces-SemiBold.ttf
+
+# BigShouldersText family
+PRODUCT_PACKAGES += \
+    BigShouldersText-Bold.ttf \
+    BigShouldersText-ExtraBold.ttf
+
+# Barlow family
+PRODUCT_PACKAGES += \
+    Barlow-Bold.ttf \
+    Barlow-Medium.ttf
+	
+#Custom packages
+PRODUCT_PACKAGES += \
+    FontGInterOverlay \
+    FontGoogleSansOverlay \
+    FontHarmonySansOverlay \
+    FontLinotteSourceOverlay \
+    FontManropeOverlay \
+    FontOnePlusSansOverlay \
+    FontOneplusSlateSourceOverlay
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/afterlife/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/afterlife/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml