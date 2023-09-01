AfterlifeOS/vendor_afterlife

Switch to new gapps also regenerate Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/common.mk
@@ -9,8 +9,8 @@ AFTERLIFE_ZIP_TYPE := Vanilla
 
 # Gapps
 ifeq ($(AFTERLIFE_GAPPS), true)
-    $(call inherit-product, vendor/gms/gms_full.mk)
-    AFTERLIFE_ZIP_TYPE := FullGapps
+    $(call inherit-product, vendor/gms/products/gms.mk)
+    AFTERLIFE_ZIP_TYPE := Gapps
 endif
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)

AuroraProject/platform_vendor_Aurora

config/common.mk
@@ -9,8 +9,8 @@ AURORA_ZIP_TYPE := Vanilla
 
 # Gapps
 ifeq ($(AURORA_GAPPS), true)
-    $(call inherit-product, vendor/gms/gms_full.mk)
-    AURORA_ZIP_TYPE := FullGapps
+    $(call inherit-product, vendor/gms/products/gms.mk)
+    AURORA_ZIP_TYPE := Gapps
 endif
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)